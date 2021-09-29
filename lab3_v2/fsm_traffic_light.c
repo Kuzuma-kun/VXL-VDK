3322635f5046b3c0b1990d9e83dc0f3f53968eff
Kuzuma-kun <55552595+Kuzuma-kun@users.noreply.github.com>
2021-09-29 17:28:36 +0700

    increase + update time for red

--- lab3_v2/fsm_traffic_light.c
+++ lab3_v2/fsm_traffic_light.c
@@ -9,16 +9,53 @@
 #include "fsm_traffic_light.h"
 
 enum Mode{NORMAL, CHANGE_RED, CHANGE_AMBER, CHANGE_GREEN};
+enum IncreaseMode{IDLE, INCREASE, INCREASE_FAST};
+
 
 enum Mode mode = NORMAL;
+enum IncreaseMode increaseMode = IDLE;
 static int blinkingCounter = 25;
+static int incCounter = 20;
+static int tempDuration = 0;
+
+void fsm_increase_mode(int *duration) {
+	switch(increaseMode) {
+	case IDLE:
+		if (getButtonValue(1) == PRESS) {
+			increaseMode = INCREASE;
+			*duration = (*duration + 1) % 100;
+		}
+		break;
+	case INCREASE:
+		if (getButtonValue(1) == RELEASE) {
+			increaseMode = IDLE;
+		} else if (getFlagButtonPress1s(1) == 1) {
+			increaseMode = INCREASE_FAST;
+			incCounter = 20;
+		}
+		break;
+	case INCREASE_FAST:
+		if (getButtonValue(1) == RELEASE) {
+			increaseMode = IDLE;
+		}
+		if (incCounter == 20) {
+			*duration = (*duration + 1) % 100;
+		}
+		incCounter--;
+		if (incCounter <= 0) incCounter = 20;
+		break;
+	}
+}
 
 void fsm_traffic_light() {
 	switch(mode) {
 	case NORMAL:
 		if (getButtonValue(0) == PRESS && getFlagButtonDelay(0) == 0) {
 			mode = CHANGE_RED;
+			increaseMode = IDLE;
+			tempDuration = red_duration;
 			setFlagButtonDelay(0);
+			blinkingCounter = 25;
 		}
 		run_traffic_light();
 		update_time_traffic_light();
@@ -27,10 +64,18 @@ void fsm_traffic_light() {
 	case CHANGE_RED:
 		if (getButtonValue(0) == PRESS && getFlagButtonDelay(0) == 0) {
 			mode = CHANGE_AMBER;
+			increaseMode = IDLE;
+			tempDuration = amber_duration;
 			setFlagButtonDelay(0);
 			blinkingCounter = 25;
 		}
-		updateBuffer(red_duration, 02);
+		fsm_increase_mode(&tempDuration);
+		updateBuffer(tempDuration, 02);
+		if (getButtonValue(1) == RELEASE && getButtonValue(2) == PRESS) {
+			red_duration = tempDuration;
+			updateBuffer(tempDuration, 88);
+		}
+		//these are used to blink the led
 		if (blinkingCounter == 25) {
 			toggleRedLight();
 		}
@@ -42,10 +87,14 @@ void fsm_traffic_light() {
 	case CHANGE_AMBER:
 		if (getButtonValue(0) == PRESS && getFlagButtonDelay(0) == 0) {
 			mode = CHANGE_GREEN;
+			increaseMode = IDLE;
 			setFlagButtonDelay(0);
+			tempDuration = green_duration;
 			blinkingCounter = 25;
 		}
-		updateBuffer(amber_duration, 03);
+		fsm_increase_mode(&tempDuration);
+		updateBuffer(tempDuration, 03);
+		//these are used to blink the led
 		if (blinkingCounter == 25) {
 			toggleAmberLight();
 		}
@@ -60,7 +109,9 @@ void fsm_traffic_light() {
 			setFlagButtonDelay(0);
 			init_traffic_light();
 		}
-		updateBuffer(green_duration, 04);
+		fsm_increase_mode(&tempDuration);
+		updateBuffer(tempDuration, 04);
+		//these are used to blink the led
 		if (blinkingCounter == 25) {
 			toggleGreenLight();
 		}
@@ -70,5 +121,4 @@ void fsm_traffic_light() {
 		}
 		break;
 	}
-
 }
